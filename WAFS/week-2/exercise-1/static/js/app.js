51f1e2e37663693febf66bf05c1f511b8e13861c
tomsnep <Snep@023.121.028.145.hva.nl>
2016-02-08 13:49:32 +0100

    sss

--- WAFS/week-2/exercise-1/static/js/app.js
+++ WAFS/week-2/exercise-1/static/js/app.js
@@ -15,7 +15,7 @@
 	
 
 	 aja()
-		  .url('https://api.instagram.com/v1/media/popular?access_token=344623690.947638a.ba610ab415254b55a7b6be050ba6435a')
+		  .url('https://api.instagram.com/v1/media/popular?access_token=806401368.f59def8.e8efe19844fd46238d592f9f20216f88')
 		  .on('success', function(data){
 		      console.log('data van amsterdam is' + data);
 		  })
