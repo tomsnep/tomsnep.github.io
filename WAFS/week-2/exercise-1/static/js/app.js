785b365072dc10063ff9c2612541b1f295b3995b
tomsnep <Snep@023.121.028.145.hva.nl>
2016-02-08 13:48:25 +0100

    asdas

--- WAFS/week-2/exercise-1/static/js/app.js
+++ WAFS/week-2/exercise-1/static/js/app.js
@@ -15,7 +15,7 @@
 	
 
 	 aja()
-		  .url('api.openweathermap.org/data/2.5/weather?id=2759794')
+		  .url('https://api.instagram.com/v1/media/popular?access_token=344623690.947638a.ba610ab415254b55a7b6be050ba6435a')
 		  .on('success', function(data){
 		      console.log('data van amsterdam is' + data);
 		  })
