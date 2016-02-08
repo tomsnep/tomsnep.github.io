39c7f433de34528aae0f164c19ee528f6b25e555
tomsnep <Snep@023.121.028.145.hva.nl>
2016-02-08 14:21:03 +0100

    fix

--- WAFS/week-2/exercise-1/static/js/app.js
+++ WAFS/week-2/exercise-1/static/js/app.js
@@ -28,8 +28,4 @@
 	   })
 	   .go();
 })();
-
-
-var node = document.createElement("<li><a target='_blank' href='" + data.data[i].link + "'><img src='" + data.data[i].images.low_resolution.url +"'></img></a></li>");                 // Create a <li> node
-        
-document.querySelector(".popular").appendChild(node);     
+  
